4a644208cab1ef29c692843a06b7f9cef29281ad
DESKTOP-OOF76T0\Admin <hoanghailethe@users.noreply.github.com>
2021-10-19 13:19:19 +0700

    pythn 2

---
+++ HackerRank/10.19/CountingValley.py
@@ -0,0 +1,47 @@
+# https://www.hackerrank.com/challenges/counting-valleys/problem?isFullScreen=false
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+
+# accepted. Score:15.0
+def countingValleys(steps, path):
+    # Write your code here
+    valley = 0
+    altitude = 0
+    for i in path:
+        if i == 'D':
+            altitude -= 1
+        elif i == 'U':
+            altitude += 1
+            if altitude == 0:
+                valley += 1
+    return valley
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
