6fc5d92fe96c8fd3186ce01ec4ab190d2901e7c4
HaiHoang <hoanghailethe@users.noreply.github.com>
2021-12-08 13:44:53 +0700

    12.8 back at practice

--- Leetcode/12.08/_860_ChangesMoney.py
+++ Leetcode/12.08/_860_ChangesMoney.py
@@ -1,26 +1,79 @@
 class Solution:
-    money_bank = {20: 0, 10: 0, 5: 0}
-    money_title = [20, 10, 5]  # ak=lready sorted
 
-    def give_back_change(self, overpay, take_money) -> int :
-        if overpay == 0:
-            money_bank[take_money] = money_bank.get(take_money) + 1
-            return 0
-
-        for money in money_title:
-            if overpay > money and money_bank.get(money) > 0 :
-                money_bank[change] = money_bank.get(change) - 1
-                return give_back_change(overpay-5, take_money)
-
-        return overpay
-
-
+    # Runtime: 4682 ms, faster than 5.04% of Python3
+    # Memory Usage: 18.2 MB, less than 80.79%
     def lemonadeChange(self, bills: List[int]) -> bool:
+        money_bank = []
+        # count_money_bank = {}
         for take_money in bills :
             overpay = take_money - 5
-            give_back_change(overpay, take_money)
+            money_bank.sort()
+            while overpay > 0 and money_bank:
+                if overpay not in money_bank:
+                    # find the first max value that less than overpay
+                    ind = len(money_bank) - 1
+                    while ind >= 0 and money_bank[ind] > overpay:
+                        ind -= 1
+                    if ind == -1:
+                        return False
+                    else:
+                        overpay -= money_bank.pop(ind)
 
+                if overpay in money_bank:
+                    money_bank.remove(overpay)
+                    overpay = 0
 
-            continue
+            if overpay > 0:
+                return False
+            else:
+                money_bank.append(take_money)
+                # print(money_bank)
+        return True
 
+    def lemonadeChange2(self, bills: List[int]) -> bool:
+
+        fives = 0
+        tens = 0
+        twenties = 0
+
+        ans = True
+
+        for el in bills:
+
+            if el == 5:
+                fives += 1
+            elif el == 10:
+                if fives > 0:
+                    tens += 1
+                    fives -= 1
+                else:
+                    ans = False
+                    break
+
+            elif el == 20:
+                if tens > 0 and fives > 0:
+                    tens -= 1
+                    fives -= 1
+                    twenties += 1
+                elif fives >= 3:
+                    fives -= 3
+                    twenties += 1
+                else:
+                    ans = False
+                    break
+        return ans
+
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        n = len(bills)
+        fives = tens = 0
+        for bill in bills:
+            if bill == 5:
+                fives += 1
+            elif bill == 10:
+                fives, tens = fives - 1, tens + 1
+            elif tens > 0:
+                fives, tens = fives - 1, tens - 1
+            else:
+                fives -= 3
+            if fives < 0: return False
         return True
