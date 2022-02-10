fd549d7e05deb8f1c2e70cd803ecfd49e1375415
HaiHoang <hoanghailethe@users.noreply.github.com>
2022-02-10 16:19:03 +0700

    thursday

--- Leetcode/_875_KokoEatBanana.py
+++ Leetcode/_875_KokoEatBanana.py
@@ -47,7 +47,14 @@ class Solution2:
 
         return minSpeed
 
-
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        low, high = 1, 10 ** 9
+        while low <= high:
+            k = (low + high) // 2
+            if sum(math.ceil(1.0 * pile / k) for pile in piles) > h: low = k + 1
+            else: high = k - 1
+        return low
     # addedTime = h - len ()   (min == 0)
     # for i = 1 -> added 
     # 5 => 8 
