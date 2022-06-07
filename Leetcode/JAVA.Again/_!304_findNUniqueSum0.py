5ae9d07c22e83624b5cf6ce2c1f24d8e9c114a98
HaiHoang <hoanghailethe@users.noreply.github.com>
2022-06-07 09:27:49 +0700

    LC.Tuesday.6.5

--- Leetcode/JAVA.Again/_!304_findNUniqueSum0.py
+++ Leetcode/JAVA.Again/_!304_findNUniqueSum0.py
@@ -1,3 +1,13 @@
 class Solution:
+#     Runtime: 45 ms, faster than 55.89% of Python3 online submissions for Find N Unique Integers Sum up to Zero.
+# Memory Usage: 14 MB, less than 16.50% of Python3 online submissions for Find N Unique Integers Sum up to Zero.
     def sumZero(self, n: int) -> List[int]:
-        # https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+        res = [] 
+        begin = 1
+        if n % 2 != 0: res.append(0)
+        for i in range(int(n/2)):
+            random_number = random.randint(begin, begin+ 9)
+            res.append(random_number)
+            res.append(0-random_number)
+            begin += 10
+        return res
