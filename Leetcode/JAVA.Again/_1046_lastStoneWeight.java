507c1d1069b6c22a5720c573ffba0352b1c2c10b
HaiHoang <hoanghailethe@users.noreply.github.com>
2022-06-29 10:42:24 +0700

    LC.Wedsnday.628

--- Leetcode/JAVA.Again/_1046_lastStoneWeight.java
+++ Leetcode/JAVA.Again/_1046_lastStoneWeight.java
@@ -2,29 +2,62 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+
+// Runtime: 4 ms, faster than 12.42% of Java online submissions for Last Stone Weight.
+// Memory Usage: 41.8 MB, less than 30.64% of Java online submissions for Last Stone Weight.
+
 public class _1046_lastStoneWeight {
     public int lastStoneWeight(int[] stones) {
-        int res = 0 ; 
-        Arrays.sort(stones, Collections.reverseOrder());
-        ArrayList<Integer> orderedStone = new ArrayList<>(Arrays.asList(stones));
-        insertStoneInOrder ( res , orderedStone );
-        
-        
-        int preStone = -1 ; 
+        // int res = 0 ; 
+        ArrayList<Integer> orderedStone = new ArrayList<>();
+        for(int stone : stones ) {
+            orderedStone.add(stone);
+        }
+        Collections.sort(orderedStone, Collections.reverseOrder()) ;
 
-        for (int stone : orderedStone) {
-            if ( orderedStone.size() == 1) break ;
-            
-            if () preStone = orderedStone.remove(0);
-            
-            res  = Math.abs(preStone - stone) ;
-            insertStoneInOrder ( res , orderedStone );
+        if(orderedStone.size() == 0) {
+            return 0 ;
+        }
+        // System.out.println(orderedStone);
+
+        while ( orderedStone.size() > 1 ) {
+            int first = orderedStone.remove(0) ;
+            int second = orderedStone.remove(0);
+
+            int dif = Math.abs(first - second) ;
+            insertStoneInOrderBinaSearch (dif, orderedStone);
         }
 
-        return res;
+        return orderedStone.get(0);
     }
 
-    private void insertStoneInOrder(int res, ArrayList<Integer> orderedStone) {
+    private void insertStoneInOrderBinaSearch(int res, ArrayList<Integer> orderedStone) {
+        if (orderedStone.size() == 0) {
+            orderedStone.add(res );
+            return ;
+        }
+        System.out.println("BEFORE " + orderedStone);
+        int begin = 0 ; 
+        int end = orderedStone.size() - 1 ;
+
+        //find where to put res inorder  :  pre <= res <= after
+        if (  res >= orderedStone.get(begin) ) orderedStone.add(0, res);
+        else if (res <= orderedStone.get(end) ) orderedStone.add(res) ;
+        else {
+            int mid = begin; 
+
+            while( ! (orderedStone.get(mid) >= res && res >= orderedStone.get(mid+1) ) ){
+                
+                mid = (begin + end ) / 2 ;
+
+                if( orderedStone.get(mid) >= res ) begin = mid ;
+                else if(orderedStone.get(mid) <=  res) end =mid ;
+            }
+
+            orderedStone.add(mid+1, res);
+
+        }
+        System.out.println("AFTER " + orderedStone);
 
     }
 }
