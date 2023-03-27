c81a6b26933aa402e5e3d11c853bb694369f7ec8
hoanghailethe <lethehoanghai@gmail.com>
2023-03-27 13:54:12 +0700

    2703_checkList&RLOS STP CMS

--- Leetcode/ONWORK/SQL/VCLIMSPRODA-480_RLOS STP CMS lost data.sql
+++ Leetcode/ONWORK/SQL/VCLIMSPRODA-480_RLOS STP CMS lost data.sql
@@ -74,3 +74,29 @@ let option2 = document.createElement("option");
 option1.text = "Kiwi"; option1.value="NH TCB";option1.selected = 'selected';
 option2.text = "VipCus2"; option2.value="NH TCB2";option2.selected = 'selected';
 x.add(option1);x.add(option2);
+
+
+-- 23/03 / Col copy from CLOS before STP  -- CREATE CAR 
+SELECT cms_collateral_id, los_security_dtl_id , SECURITY_NO  FROM CMS_SECURITY WHERE security_sub_type_id = 'MS607' ORDER BY cms_collateral_id DESC; 
+SELECT cms_collateral_id, los_security_dtl_id , SECURITY_NO  FROM CMS_SECURITY WHERE security_sub_type_id = 'MS606' ORDER BY cms_collateral_id DESC; 
+SELECT cms_collateral_id, los_security_dtl_id , SECURITY_NO  FROM CMS_SECURITY WHERE security_sub_type_id = 'MS706' ORDER BY cms_collateral_id DESC;  
+SELECT cms_collateral_id, los_security_dtl_id , SECURITY_NO  FROM CMS_SECURITY WHERE security_sub_type_id = 'MS709' ORDER BY cms_collateral_id DESC;  
+SELECT cms_collateral_id, los_security_dtl_id , SECURITY_NO  FROM CMS_SECURITY WHERE security_sub_type_id = 'MS610' ORDER BY cms_collateral_id DESC; 
+SELECT cms_collateral_id, los_security_dtl_id , SECURITY_NO  FROM CMS_SECURITY WHERE security_sub_type_id = 'MS700' ORDER BY cms_collateral_id DESC;  
+SELECT cms_collateral_id, los_security_dtl_id , SECURITY_NO  FROM CMS_SECURITY WHERE security_sub_type_id = 'MS601' ORDER BY cms_collateral_id DESC;  
+SELECT cms_collateral_id, los_security_dtl_id , SECURITY_NO  FROM CMS_SECURITY WHERE security_sub_type_id = 'PT703' ORDER BY cms_collateral_id DESC;  
+SELECT cms_collateral_id, los_security_dtl_id , SECURITY_NO  FROM CMS_SECURITY WHERE security_sub_type_id = 'GT408' ORDER BY cms_collateral_id DESC; 
+SELECT cms_collateral_id, los_security_dtl_id , SECURITY_NO  FROM CMS_SECURITY WHERE security_sub_type_id = 'OT900' ORDER BY cms_collateral_id DESC;  
+SELECT cms_collateral_id, los_security_dtl_id , SECURITY_NO  FROM CMS_SECURITY WHERE security_sub_type_id = 'MS611' ORDER BY cms_collateral_id DESC; 
+--20190123000029583	201901230000030331	MS201901230000030331   MS607
+--20170220000028674	201703230000029332	MS201702200000029085  MS606
+--20190611000029847	201904220000031294	MS201904220000031294   MS610
+-- 20190123000029591	201901230000030335	MS201901230000030335   MS601
+-- 20170220000028662	201703230000029322	PT201702200000029077   PT703
+-- 20190123000029589	201901230000030334	GT201901230000030334   GT408
+-- 20190618000029865	201906180000031505	OT201906180000031505   OT900
+-- 20190123000029583	201901230000030331	MS201901230000030331    MS611
+
+Type of CAR *	Personal Loan  
+Application Number ^	CAR/23/0001/00122/00009331 
+
