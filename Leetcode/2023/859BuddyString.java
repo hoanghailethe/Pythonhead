17d45ac8d7c878bd12ad1c4875e84d04aafd1eba
hoanghailethe <lethehoanghai@gmail.com>
2023-07-04 17:32:31 +0700

    j bo score

--- Leetcode/2023/859BuddyString.java
+++ Leetcode/2023/859BuddyString.java
@@ -42,4 +42,78 @@ public class 859BuddyString {
         }    
         return false ;
     }
+
+        public boolean buddyStrings2(String s, String goal) {
+
+            int n = s.length();
+            if (s.equals(goal)) {
+                Set<Character> temp = new HashSet<>();
+                for (char c : s.toCharArray()) {
+                    temp.add(c);
+                }
+                return temp.size() < goal.length(); // Swapping same characters
+            }
+
+            int i = 0;
+            int j = n - 1;
+
+            while (i < j && s.charAt(i) == goal.charAt(i)) {
+                i++;
+            }
+
+            while (j >= 0 && s.charAt(j) == goal.charAt(j)) {
+                j--;
+            }
+
+            if (i < j) {
+                char[] sArr = s.toCharArray();
+                char temp = sArr[i];
+                sArr[i] = sArr[j];
+                sArr[j] = temp;
+                s = new String(sArr);
+            }
+
+            return s.equals(goal);
+        }
+
+        public boolean buddyStrings3(String s, String goal) {
+            if(s == null || goal == null || s.length() != goal.length() || s.length() < 2){
+                return false;
+            }
+            if(s.equals(goal)){
+                int[] map = new int[26];
+                for(int i = 0; i < s.length(); i++){
+                    if(map[s.charAt(i) - 'a'] == 1){
+                        return true;
+                    }else{
+                        map[s.charAt(i) - 'a'] = 1;
+                    }
+                }
+            }
+            char[] s1 = s.toCharArray();
+            char[] s2 = goal.toCharArray();
+            char s1_1 = '1', s1_2 = '2', s2_1 = '3', s2_2 = '4';
+            boolean found1stDiff = false;
+            boolean found2ndDiff = false;
+
+            for(int i = 0; i < s1.length; i++){
+                if(s1[i] != s2[i]){
+                    if(!found1stDiff){
+                        found1stDiff = true;
+                        s1_1 = s1[i];
+                        s2_1 = s2[i];
+                    }else if(!found2ndDiff){
+                        found2ndDiff = true;
+                        s1_2 = s1[i];
+                        s2_2 = s2[i];
+                        if(s1_2 != s2_1 || s1_1 != s2_2){
+                            return false;
+                        }
+                    }else{
+                        return false;
+                    }
+                }
+            }
+            return found1stDiff && found2ndDiff;
+        }
 }
