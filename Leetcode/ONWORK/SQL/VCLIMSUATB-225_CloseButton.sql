b3cc68f4d78032fe801c99391528f0f9efb7dcff
hoanghailethe <lethehoanghai@gmail.com>
2023-07-06 17:39:12 +0700

    j close butin

---
+++ Leetcode/ONWORK/SQL/VCLIMSUATB-225_CloseButton.sql
@@ -0,0 +1,27 @@
+
+View Security Details - Deposit
+General
+ 	
+Collateral ID  *^	CS20140314013053
+
+The transaction has been deleted.
+Your Transaction ID is 20230706000255612
+6.7.2023 4.34.20 PM
+
+
+
+--
+CHECKER APPROVE
+The transaction has been approved.
+Your Transaction ID is 20230706000255613
+6.7.2023 4.35.56 PM
+
+--MAKER CHECK AGAIN
+
+SELECT CMS_OUTSTANDING_AMT from SCI_LSP_APPR_LMTS
+    where CMS_LIMIT_PROFILE_ID = ?
+    AND cms_lsp_appr_lmts_id = ?
+    and CMS_LIMIT_STATUS = 'ACTIVE';
+
+
+    CMS_LIMIT_CHARGE_MAP
