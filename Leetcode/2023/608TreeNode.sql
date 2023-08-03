a7aeea755315f0840ff3eebe0438141efd6759ac
Hai Hoang <lethehoanghai@gmail.com>
2023-08-03 17:40:31 +0700

    LC thursday sql

--- Leetcode/2023/608TreeNode.sql
+++ Leetcode/2023/608TreeNode.sql
@@ -1,2 +1,25 @@
-SELECT id 
-FROM
+
+SELECT id,
+CASE 
+    WHEN p_id IS NULL THEN 'Root'
+    WHEN id NOT IN (SELECT DISTInCT(p_id ) FROM TREE) THEN 'Leaf'
+    ELSE 'Inner'
+END AS type
+ FROM Tree ;
+
+
+
+SELECT id,
+CASE 
+    WHEN p_id IS NULL THEN 'Root'
+    WHEN id NOT IN (SELECT DISTInCT(p_id ) FROM TREE WHERE p_id IS NOt NULL) THEN 'Leaf'
+    ELSE 'Inner'
+END AS type
+ FROM Tree ;
+
+963ms
+Beats 82.04%of users with MySQL
+Memory
+Details
+-mb
+Beats 100.00%of users with MySQL
